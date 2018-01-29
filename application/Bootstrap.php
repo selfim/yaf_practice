4ea372c6a11e868b55e35eaa299a9eec1d321688
selfim <lm233427@gmail.com>
2018-01-29 22:04:31 +0800

    add a route rule

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -23,6 +23,18 @@ class Bootstrap extends Yaf_Bootstrap_Abstract {
 
 	public function _initRoute(Yaf_Dispatcher $dispatcher) {
 		//在这里注册自己的路由协议,默认使用简单路由
+		//创建一个路由协议实例
+		$router = Yaf_Dispatcher::getInstance()->getRouter();
+		$route = new Yaf_Route_Rewrite(
+			'user/reg/:username',
+			array(
+			'controller'=>'user',
+			'action'=>'reg'
+			)
+
+		);
+		//使用路由器装载路由协议
+     $router->addRoute('product', $route);
 	}
 	
 	public function _initView(Yaf_Dispatcher $dispatcher) {
