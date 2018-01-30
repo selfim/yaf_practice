dd5d2a708eb3028207333ae1212929683c105d37
selfim <lm233427@gmail.com>
2018-01-30 22:32:41 +0800

    login action add

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @name IndexController
- * @author limeng\limeng
+ * @author leo
  * @desc 默认控制器
  * @see http://www.php.net/manual/en/class.yaf-controller-abstract.php
  */
@@ -33,9 +33,49 @@ class UserController extends Yaf_Controller_Abstract {
 	public function indexAction(){
 		return $this->loginAction();
 	}
-
+	//登录api实现
 	public function loginAction(){
+		//防爬虫
+		$submit = $this->getRequest()->getQuery("submit","0");
+		if($submit!="1"){
+			echo json_encode(array('code'=>-104,'msg'=>"请通过正确方式提交！"));
+			return FALSE;
+		}
+
+		//获取参数
+		$name = $this->getRequest()->getPost('name',false);
+		$pwd = $this->getRequest()->getPost('pwd',false);
+		if(!$name || !$pwd){
+			echo json_encode(array("code"=>-100,"msg"=>"用户名和密码必填"));
+			return false;
+		}
+
+		//调用model 登录验证
+		$model = new UserModel();
+		$info = $model->login(trim($name),trim($pwd));
+		if($info){
+			//SESSION
+			/**
+			session_start();
+			$_SESSION['user_token'] = md5("SALT".$_SERVER['REQUEST_TIME'].$info);
+			$_SESSION['user_token_time'] = $_SERVER['REQUEST_TIME'];
+			$_SESSION['user_id'] = $info;
+			*/
+			echo json_encode(array(
+				"code"=>0,
+				"msg"=>"",
+				"data"=>array(
+				  "name"=>$name
+				)
+			));
+		}else{
 		
+			echo json_encode(array(
+				"code"=>$model->code,
+				"msg"=>$model->msg,
+			));
+		}
+		return TRUE;
 	}
 
 	public function registerAction(){
