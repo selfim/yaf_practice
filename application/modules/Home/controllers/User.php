23718c78c9be586151df6a520ef70116a7281df0
selfim <lm233427@gmail.com>
2018-04-01 22:33:03 +0800

    excelexport

--- application/modules/Home/controllers/User.php
+++ application/modules/Home/controllers/User.php
@@ -1,19 +1,22 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: limeng
+ * User: leo
  * Date: 2018/3/31
  * Time: 23:12
  */
-require_once(__DIR__.'/../../../library/ThirdParty/PHPExcel.php');
+
 class UserController extends Yaf_Controller_Abstract
 {
     public function testAction()
     {
-        $phpexcelModel = new PHPExcel();
 
-        var_dump($phpexcelModel);
-        echo 'This is home module test action';
+        $model = new UserModel();
+
+        $rst = $model->exportUser();
+        #var_dump($model);
+        #echo 'This is home module test action';
+
         return false;//Yaf默认会去加载视图，return false是为了不让它自动加载视图。
     }
 }
