ff0dd4b4e5182ba36ce825962ef335206a23b38d
selfim <lm233427@gmail.com>
2018-04-22 22:33:14 +0800

    add vendor

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-
+//加上下面这一句话，用于自动加载QueryList
+require 'vendor/autoload.php';
 define('APPLICATION_PATH', dirname(__FILE__));
 
 $application = new Yaf_Application( APPLICATION_PATH . "/conf/application.ini");
