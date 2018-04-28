7f211627a31223146c35bc278887cb8eb2230146
selfim <lm233427@gmail.com>
2018-04-29 00:15:41 +0800

    add workerman

---
+++ application/controllers/request.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: limeng
+ * Date: 2018/4/28
+ * Time: 23:57
+ */
+#require_once '../../vendor/workerman/workerman/Autoloader.php';
+require_once '../../vendor/autoload.php';
+use Workerman\Worker;
+
+// #### http worker ####
+$http_worker = new Worker("http://0.0.0.0:2345");
+
+// 4 processes
+$http_worker->count = 4;
+
+// Emitted when data received
+$http_worker->onMessage = function($connection, $data)
+{
+    // $_GET, $_POST, $_COOKIE, $_SESSION, $_SERVER, $_FILES are available
+    var_dump($_GET, $_POST, $_COOKIE, $_SESSION, $_SERVER, $_FILES);
+    // send data to client
+    $connection->send("hello world \n");
+};
+
+// run all workers
+Worker::runAll();
